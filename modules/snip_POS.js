25f4730bab4431771b2e4ebac471b7b6cafd0842
lihy <lhy_9005@163.com>
2017-05-04 15:56:22 +0800

    修改pos截图，先提交一个版本

--- modules/snip_POS.js
+++ modules/snip_POS.js
@@ -8,10 +8,12 @@ var Image = Canvas.Image;
 var glob = require("glob");
 var perLatLines = 0;  //每个纬度用多少行
 var perLonLines = 0;  //每个经度用多少行
-var blockLat = 0;
-var blockLon = 0;
-var latScope = 0;
-var lonScope = 0;
+var blockLat = 0; //在纬度上分几块
+var blockLon = 0; //在经度上分几块
+var latScope = 0; //每个块的纬度跨度
+var lonScope = 0; //每个块的经度跨度
+var perBlockWidth = 0;//每个分块的宽度，在配置文件中已经定义好
+var perBlockHeight = 0;//每个分块的高度，在配置文件中已经定义好
 /**
  * 截图
  * @param res 
@@ -28,7 +30,8 @@ var lonScope = 0;
 exports.snipImage_GLL = function (res,config,outPutPath,t_l_lat,t_l_lon,b_r_lat,b_r_lon,dateTime,fileType, next) {
     console.log("snipImage_GLL start");
     //查找原图文件列表
-    var filterKey = config.Key1 + "*" + config.Res + "*" + config.key2 + "*." + fileType;
+    var filterKey = config.key1 + "*" + config.Res + "*" + config.key2 + "*" + fileType;
+    console.log(filterKey);
     var tmpBasePath = config.BasePath + "/" + dateTime + "/GLL/";
     glob(tmpBasePath + filterKey, function (err, files) {
         if(err){
@@ -38,19 +41,49 @@ exports.snipImage_GLL = function (res,config,outPutPath,t_l_lat,t_l_lon,b_r_lat,
         lonScope = config.LonScope;
         blockLat = 180 / config.LatScope;
         blockLon = 360 / config.LonScope;
-        perLatLines = config.Height / config.LatScope;
-        perLonLines = config.Width / config.LonScope;
+        perBlockHeight = config.Height;
+        perBlockWidth = config.Width;
+        perLatLines = perBlockHeight / config.LatScope;
+        perLonLines = perBlockWidth / config.LonScope;
         var shotLatScope = t_l_lat - b_r_lat;
         var shotLonScope = b_r_lon - t_l_lon;
         var cutWidth = Math.floor(shotLonScope * perLonLines);
         var cutHeight = Math.floor(shotLatScope * perLatLines);
         //获取待截图列表
         var shotFiles = _getFiles(files,t_l_lat,t_l_lon,b_r_lat,b_r_lon);
-        if (!isFind) {
+        shotFiles.sort();
+        if (shotFiles.length == 0) {
             next("find error", null);
         }
+        else{
+            var canvas = new Canvas(cutWidth,cutHeight);
+            var ctx = canvas.getContext('2d');
+            var tmpWidth = 0;
+            var tmpHeight = 0;
+            shotFiles.forEach(function (item) {
+                    var buf = fs.readFileSync(item);
+                    var srcImg = new Image;
+                    srcImg.src = buf;
+                    if (tmpWidth + item.width <= cutWidth) {
+                        if (!isErr) {
+                            ctx.drawImage(srcImg, tmpWidth * -1, tmpHeight * -1, item.width, item.height);
+                        }
+                        tmpWidth += item.width;
+                    }
+                    else {
+                        tmpWidth = 0;
+                        tmpHeight += item.height;
+                        if (!isErr) {
+                            ctx.drawImage(srcImg, tmpWidth * -1, tmpHeight * -1, item.width, item.height);
+                        }
+                    }
+                }
+            );
+            console.log("draw ok");
+            next(null, canvas.toBuffer());
+        }
     });
-}
+};
 
 /**
  * 获取在截图区域内的所有文件
@@ -92,27 +125,87 @@ function ItemModel() {
     self.tNo = 0;
     self.check = function (l_t_lat,l_t_lon,r_b_lat,r_b_lon) {
         var block_l_t_lat,block_l_t_lon,block_r_b_lat,block_r_b_lon;
-        block_l_t_lat = 90 - (self.tNo / blockLon) * latScope;
-        block_l_t_lon = -180 + (self.tNo % blockLon) * lonScope;
-        block_r_b_lat = block_l_t_lat + latScope;
-        block_r_b_lon = block_l_t_lon + lonScope;
+        block_l_t_lat = 90 - parseInt(self.tNo / blockLon) * latScope;
+        block_l_t_lon = -180 + parseInt(self.tNo % blockLon) * lonScope;
+        block_r_b_lat = Number(block_l_t_lat) - Number(latScope);
+        block_r_b_lon = Number(block_l_t_lon) + Number(lonScope);
 
         //首先判断是否不在截取范围内
-        if(block_r_b_lat >= l_t_lat && block_r_b_lon <= l_t_lon){
+        //对超出截图区域的忽略
+        if((block_l_t_lat <= r_b_lat) ||
+            (block_r_b_lat >= l_t_lat) ||
+            (block_r_b_lon <= l_t_lon) ||
+            (block_l_t_lon >= r_b_lon)){
             return false;
         }
-        if(block_l_t_lat <= r_b_lat && block_l_t_lon >= r_b_lon){
-            return false;
-        }
-        inShot = true;
-        if(block_l_t_lat <= l_t_lat && block_l_t_lon >=l_t_lon && block_r_b_lat >= r_b_lat && block_r_b_lon <= r_b_lon){
+        // if((block_r_b_lat >= l_t_lat && block_r_b_lon <= l_t_lon) ||  //10°块右下角经纬度小于截图左上角
+        //     (block_l_t_lat <= r_b_lat && block_l_t_lon >= r_b_lon) || //10°块左上角经纬度大于截图右下角
+        //     (block_l_t_lat <= r_b_lat && block_r_b_lon <= l_t_lon) || //10°块右上角经纬度小于截图左下角
+        //     (block_r_b_lat >= l_t_lat && block_l_t_lon >= r_b_lon)){  //10°块左下下角经纬度大于截图右上角
+        //     return false;
+        // }
+        //该10°块文件完全包含在截图范围内
+        if(block_l_t_lat <= l_t_lat && block_l_t_lon >=l_t_lon && block_r_b_lat >= r_b_lat && block_r_b_lon <= r_b_lon) {
             self.iOffset = 0;
             self.jOffset = 0;
-            self.width = lonScope * perLonLines;
-            self.height = latScope * perLatLines;
+            self.width = perBlockWidth;
+            self.height = perBlockHeight;
         }
         else{
-
+            //10°块经度范围包含了左上角经度,说明该10°块文件在截图区域的左边线上
+            if(block_l_t_lon < l_t_lon && block_r_b_lon > l_t_lon){
+                self.iOffset = (l_t_lon - block_l_t_lon) * perLonLines;
+                self.width = perBlockWidth - self.iOffset;
+                //如果10°块文件在截图左侧线上，不包含左上角和左下角
+                if(block_l_t_lat <= l_t_lat && block_r_b_lat >= r_b_lat){
+                    self.jOffset = 0;
+                    self.height = perBlockHeight;
+                }
+                //如果10°块文件在截图左侧线上，包含截图区域左上角
+                else if(block_l_t_lat > l_t_lat && block_r_b_lat < l_t_lat){
+                    self.jOffset = (block_l_t_lat - l_t_lat) * perLatLines;
+                    self.height = perBlockHeight - self.jOffset;
+                }
+                //如果10°块文件在截图左侧线上，包含截图区域左下角
+                else{
+                    self.jOffset = 0;
+                    self.height = (block_l_t_lat - r_b_lat) * perLatLines;
+                }
+            }
+            //10°块经度范围包含了右下角经度,说明该10°块文件在截图区域的右边线上
+            else if(block_l_t_lon < r_b_lon && block_r_b_lon > r_b_lon){
+                self.iOffset = (r_b_lon - block_l_t_lon) * perLonLines;
+                self.width = perBlockWidth - self.iOffset;
+                //如果10°块文件在截图右侧线上，不包含右上角和右下角
+                if(block_l_t_lat <= l_t_lat && block_r_b_lat >= r_b_lat){
+                    self.jOffset = 0;
+                    self.height = perBlockHeight;
+                }
+                //如果10°块文件在截图右侧线上，包含截图区域右上角
+                else if(block_l_t_lat > l_t_lat && block_r_b_lat < l_t_lat){
+                    self.jOffset = (block_l_t_lat - l_t_lat) * perLatLines;
+                    self.height = perBlockHeight - self.jOffset;
+                }
+                //如果10°块文件在截图右侧线上，包含截图区域右下角
+                else{
+                    self.jOffset = 0;
+                    self.height = (block_l_t_lat - r_b_lat) * perLatLines;
+                }
+            }
+            //10°块经度范围包含了左上角纬度,说明该10°块文件在截图区域的上边线上
+            else if(block_l_t_lat > l_t_lat && block_r_b_lat < l_t_lat){
+                self.iOffset = 0;
+                self.jOffset = (block_l_t_lat - l_t_lat) * perLatLines;
+                self.width = perBlockWidth;
+                self.height = perBlockHeight - self.jOffset;
+            }
+            //10°块经度范围包含了右下角纬度,说明该10°块文件在截图区域的下边线上
+            else if(block_l_t_lat > r_b_lat && block_r_b_lat < r_b_lat){
+                self.iOffset = 0;
+                self.jOffset = 0;
+                self.width = perBlockWidth;
+                self.height = (block_l_t_lat - r_b_lat) * perLatLines;
+            }
         }
         return true;
     }
